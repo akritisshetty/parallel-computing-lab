4b52f9447a77377c6ad6a743d630bccdc4b508ff
Akriti S Shetty <akritishetty.in@gmail.com>
2026-09-02 18:36:32 +0530

    Add files via upload

---
+++ parallelPrimeNum4.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int is_prime(int x)
+{
+    if (x < 2)
+        return 0;
+
+    for (int d = 2; d * d <= x; d++)
+    {
+        if (x % d == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Usage: %s <n> <threads>\n", argv[0]);
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+    int threads = atoi(argv[2]);
+
+    omp_set_num_threads(threads);
+
+    int count;
+    double t0, t1;
+
+    // Serial
+    count = 0;
+
+    t0 = omp_get_wtime();
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (is_prime(i))
+            count++;
+    }
+
+    t1 = omp_get_wtime();
+
+    printf("Serial: %d primes, %f sec\n", count, t1 - t0);
+
+    // Parallel
+    count = 0;
+
+    t0 = omp_get_wtime();
+
+    #pragma omp parallel for reduction(+:count)
+    for (int i = 1; i <= n; i++)
+    {
+        if (is_prime(i))
+            count++;
+    }
+
+    t1 = omp_get_wtime();
+
+    printf("Parallel (%d threads): %d primes, %f sec\n",
+           threads, count, t1 - t0);
+
+    return 0;
+}
